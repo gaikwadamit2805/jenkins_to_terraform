679512eb03dca096e00ff59d0f58e49dbd6a5aec
gaikwadamit2805 <138217865+gaikwadamit2805@users.noreply.github.com>
2024-07-30 16:18:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" #  region
+}
+
+resource "aws_instance" "example" {
+  count         = 10
+  ami           = "ami-0427090fd1714168b" # Specify your AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance-${count.index}"
+  }
+}
